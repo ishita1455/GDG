9e278a660767577fc6eb4e3f63363ade6d92588f
ishita1455 <shegaonkarishita@gmail.com>
2024-09-29 21:17:26 +0530

    Update gdg.js

--- gdg.js
+++ gdg.js
@@ -5,22 +5,44 @@ const faqQuestions = document.querySelectorAll('.faq-question');
 const faqAnswers = document.querySelectorAll('.faq-answer');
 const techGridItems = document.querySelectorAll('.tech-grid div');
 
-// Toggle Dark Mode
+
 toggleThemeButton.addEventListener('click', function () {
     body.classList.toggle('dark-mode');
     header.classList.toggle('dark-mode');
 
-    // Toggle dark mode for each FAQ question and answer
+    
     faqQuestions.forEach(faq => faq.classList.toggle('dark-mode'));
     faqAnswers.forEach(answer => answer.classList.toggle('dark-mode'));
 
-    // Toggle dark mode for each technology grid item
     techGridItems.forEach(item => item.classList.toggle('dark-mode'));
 
-    // Update button text
+   
     if (body.classList.contains('dark-mode')) {
         toggleThemeButton.textContent = 'Switch to Light Mode';
     } else {
         toggleThemeButton.textContent = 'Switch to Dark Mode';
     }
 });
+
+
+function autoScroll(scrollSpeed) {
+    window.scrollBy(0, scrollSpeed); 
+    if (window.scrollY >= document.body.scrollHeight - window.innerHeight) {
+        window.scrollTo(0, 0);
+    } else {
+        setTimeout(() => autoScroll(scrollSpeed), 100); 
+    }
+}
+
+let userScrolled = false;
+
+window.addEventListener('scroll', () => {
+    userScrolled = true;
+});
+
+setTimeout(() => {
+    if (!userScrolled) {
+        const scrollSpeed = 40; 
+        autoScroll(scrollSpeed);
+    }
+}, 1000); 
